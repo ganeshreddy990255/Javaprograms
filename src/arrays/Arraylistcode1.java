0edf46ef7122c71329ac32de8aa7311d25fc97e3
Ganesh reddy g <Ganesh reddy g@ganesh_reddy990>
2024-11-08 11:42:18 +0530

    arraylist

---
+++ src/arrays/Arraylistcode1.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.ArrayList;
+
+public class Arraylistcode1 {
+public static void main(String[] args) {
+	ArrayList al =new ArrayList();
+	al.add(100);
+	al.add(50);
+	al.add(150);
+	al.add(25);
+	al.add(75);
+	al.add(125);
+	al.add(175);
+	
+	System.out.println(al);
+	
+	al.add("Ganeshreddy");
+	al.add("punith");
+	al.add("Rakesh");
+	al.add(78.89f);
+	al.add(true);
+	 System.out.println(al);
+	 
+	 al.add(3,4000);
+	 
+	 ArrayList al1 =new ArrayList();
+	 al1.add(1);
+	 al1.add(5);
+	 al1.add(15);
+	 al1.add(2);
+	 al1.add(7);
+	 al1.add(1);
+	 al1.add(17);
+	 System.out.println(al);
+	 
+	
+	
+	
+}
+}
